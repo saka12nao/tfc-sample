d60c11c3b6d9fb03a491a330c99b949ce851c9f0
たなかともあき <86752616+vtj-ttanaka@users.noreply.github.com>
2022-05-27 18:11:08 +0900

    add files

---
+++ main.tf
@@ -0,0 +1,21 @@
+variable "project" {}
+
+provider "google" {
+  project = var.project
+}
+
+resource "google_compute_instance" "default" {
+  name         = "tfc-sample"
+  machine_type = "e2-medium"
+  zone         = "asia-northeast1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
